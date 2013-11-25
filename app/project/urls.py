a6c1bbbc3e0f7005c93cc1786ab0f40862962bf8
S.Mojtaba Banaie <mojtaba.banaie@gmail.com>
2013-11-25 05:58:22 +0330

    Smbanaie Changes to quickly start over....

--- app/project/urls.py
+++ app/project/urls.py
@@ -6,7 +6,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    # url(r'^$', TemplateView.as_view(template_name='homepage/home.html')),
+    url(r'^$', 'views.home'),
 
     # Examples:
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
